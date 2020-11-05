f8e599f139eafce791bfebdf983babbec89e550c
lucaswei <lucaswei@google.com>
2020-11-05 16:24:27 +0800

    qcom: qpnp-pq: Fix -Wpointer-to-int-cast
    drivers/power/supply/qcom/qpnp-qg.c:5110:21: error: cast to smaller integer type 'u8' (aka 'unsigned char') from 'const void *' [-Werror,-Wvoid-pointer-to-int-cast]
            chip->qg_version = (u8)of_device_get_match_data(&pdev->dev);
                               ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    Fix `chip->qg_version = (u8)of_device_get_match_data(&pdev->dev)`
    by casting pointer value to enum qg_version instead of truncated
    int type.
    
    Bug: 171827315
    Signed-off-by: lucaswei <lucaswei@google.com>
    Change-Id: I1dd1eb91bda9b44ca640a2c89c94d158abff5e7f

--- drivers/power/supply/qcom/qpnp-qg.c
+++ drivers/power/supply/qcom/qpnp-qg.c
@@ -5714,7 +5714,7 @@ static int qpnp_qg_probe(struct platform_device *pdev)
 	chip->max_verify_psy = power_supply_get_by_name("batt_verify");
 #endif
 
-	chip->qg_version = (u8)of_device_get_match_data(&pdev->dev);
+	chip->qg_version = (enum qg_version)of_device_get_match_data(&pdev->dev);
 
 	switch (chip->qg_version) {
 	case QG_LITE:
